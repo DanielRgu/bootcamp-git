387d5a5d88ecf8f4192a81d33d41242fcd962057
Daniel Abercrombie <d.abercrombie1@rgu.ac.uk>
2026-09-24 11:45:40 +0100

    added second for loop

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ console.log("h");
 for (let i = 103; i < 1023282646473; i = i **2 + 100*i){
     console.log(i);
 }
+
+for (let i = 10; i < 102933448; i += 300**i){
+    console.log(i);
+}
+
+console.log("Hi")
